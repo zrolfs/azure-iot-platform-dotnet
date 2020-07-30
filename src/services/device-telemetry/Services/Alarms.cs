68a35fce8f1592a4a518c314f3d2f29f2e0aff70
Viswanadh Kotha <Viswanadh.kotha@ggktech.com>
2020-07-30 18:24:37 +0530

    Updated ResourceNotFoundException in Rules Methods

--- src/services/device-telemetry/Services/Alarms.cs
+++ src/services/device-telemetry/Services/Alarms.cs
@@ -172,9 +172,9 @@ namespace Mmm.Iot.DeviceTelemetry.Services
                         .Select(doc => new Alarm(doc))
                         .ToList();
             }
-            catch (ResourceNotFoundException e)
+            catch (ResourceNotFoundException)
             {
-                throw new ResourceNotFoundException($"No alarms exist in CosmosDb. The alarms collection {this.CollectionId} does not exist.", e);
+                return new List<Alarm>();
             }
         }
 
@@ -213,9 +213,9 @@ namespace Mmm.Iot.DeviceTelemetry.Services
                     queryOptions,
                     sql);
             }
-            catch (ResourceNotFoundException e)
+            catch (ResourceNotFoundException)
             {
-                throw new ResourceNotFoundException($"No alarms exist in CosmosDb. The alarms collection {this.CollectionId} does not exist.", e);
+                return 0;
             }
         }
 
